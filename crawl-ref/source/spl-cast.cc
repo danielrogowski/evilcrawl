9a364fa81d752ccf43b735c3207e041ce71dd5bf
Rogo <d.rogowski@gmx.de>
2023-05-23 21:51:52 +0200

    show spell power as percentage

--- crawl-ref/source/spl-cast.cc
+++ crawl-ref/source/spl-cast.cc
@@ -2233,15 +2233,6 @@ int power_to_barcount(int power)
     return breakpoint_rank(power, breakpoints, ARRAYSZ(breakpoints)) + 1;
 }
 
-static int _spell_power_bars(spell_type spell)
-{
-    const int cap = spell_power_cap(spell);
-    if (cap == 0)
-        return -1;
-    const int power = min(calc_spell_power(spell, true, false, false), cap);
-    return power_to_barcount(power);
-}
-
 #ifdef WIZARD
 static string _wizard_spell_power_numeric_string(spell_type spell)
 {
@@ -2253,6 +2244,15 @@ static string _wizard_spell_power_numeric_string(spell_type spell)
 }
 #endif
 
+int spell_power_percent(spell_type spell)
+{
+    const int pow = calc_spell_power(spell, true, false);
+    const int max_pow = spell_power_cap(spell);
+    if (max_pow == 0)
+        return -1; // should never happen for player spells
+    return pow * 100 / max_pow;
+}
+
 string spell_power_string(spell_type spell)
 {
 #ifdef WIZARD
@@ -2260,13 +2260,11 @@ string spell_power_string(spell_type spell)
         return _wizard_spell_power_numeric_string(spell);
 #endif
 
-    const int numbars = _spell_power_bars(spell);
-    const int capbars = power_to_barcount(spell_power_cap(spell));
-    ASSERT(numbars <= capbars);
-    if (numbars < 0)
+    const int percent = spell_power_percent(spell);
+    if (percent < 0)
         return "N/A";
     else
-        return string(numbars, '#') + string(capbars - numbars, '.');
+        return make_stringf("%d%%", percent);
 }
 
 int calc_spell_range(spell_type spell, int power, bool allow_bonus)
